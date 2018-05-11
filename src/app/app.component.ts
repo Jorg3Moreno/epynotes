56c15bfe36c37770c96b30b12f51db67d696f0d3
jorgemoreno <6826544+Jorg3Moreno@users.noreply.github.com>
2018-05-11 17:59:59 -0500

    Service worker version management using 'SwUpdate'.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {SwUpdate} from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  // swUpdate to handle/notify when service worker need be updated
+  constructor (private swUpdate: SwUpdate) {
+  }
+
+  ngOnInit(): void {
+    // check if web browser supports service worker functionality
+    if (this.swUpdate.isEnabled) {
+      // available: to check and notify if is there is a new version available of sw
+      this.swUpdate.available.subscribe(() => {
+        window.location.reload();
+      });
+    }
+  }
 }
